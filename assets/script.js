300b778d2a5f25f5fcb532c5bcd88f667cc283f6
Glatigny <f.west@live.fr>
2019-01-29 09:35:21 +0100

    exo 4

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
 function valid(champ)
 {
   var password = document.getElementById("password").value;
-  var confirmPasswordb = document.getElementById("confirmPassword").value;
+  var confirmPassword = document.getElementById("confirmPassword").value;
 
   if (password != confirmPassword) {
     document.getElementById("confirmPassword").style.borderColor="red";
       document.getElementById("password").style.borderColor="red";
+  }else{
+    document.getElementById("confirmPassword").style.borderColor="green";
+      document.getElementById("password").style.borderColor="green";
   }
 }
